d81f37b4e9b46eb5721275d57001fe4aa795f244
Chris Kokolios <kokolios1986@yahoo.gr>
2016-03-26 17:12:53 +0200

    start server

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var express = require('express'),
-    app     = express();
+    app     = express(),
+    port 	= process.env.PORT || 3000;
+
+
+app.get("/",function (req,res) {
+	console.log(req.headers)
+
+});
 
 
 
+app.listen(port,function(){
+	console.log("Server Started on port " + 3000);
+})
